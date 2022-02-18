cd135f61250251e7bbb39164206c4741e28e2caf
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2022-02-18 09:44:32 -0800

    Avoid false positives for Other roll/casts/obtains
    Signed-off-by: Nadya <4567166+NadyaNayme@users.noreply.github.com>

--- TidyChat/ChatFilters/FilterLootMessages.cs
+++ TidyChat/ChatFilters/FilterLootMessages.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace TidyChat
 {
@@ -13,9 +12,21 @@ namespace TidyChat
                     !configuration.ShowLootRoll && Localization.Get(ChatRegexStrings.RollsNeedOrGreed).IsMatch(input) ||
                     !configuration.ShowCastLot && Localization.Get(ChatRegexStrings.CastLot).IsMatch(input) ||
                     !configuration.ShowObtainedShards && Localization.Get(ChatRegexStrings.ObtainedShards).IsMatch(input) ||
-                    !configuration.ShowOthersLootRoll && Localization.Get(ChatRegexStrings.OthersRollNeedOrGreed).IsMatch(input) ||
-                    !configuration.ShowOthersCastLot && Localization.Get(ChatRegexStrings.OthersCastLot).IsMatch(input) ||
-                    !configuration.ShowOthersObtain && Localization.Get(ChatRegexStrings.OthersObtain).IsMatch(input)
+                    /* TODO: #1
+                     * Benchmark zero-width negative lookbehind against StartsWith()
+                     * I'm like 99% certain StartsWith will be faster
+                     * 
+                     * TODO: #2
+                     * Double check if this is even necessary to avoid false positives with your own Loot/Cast rolls
+                     * The goal is to not show "You cast your lot" while still showing "Other Player" casts their lot
+                     * when the user has changed the chat log from the Full Name setting to an initialized setting
+                     * and has not enabled showing their own loot/cast rolls but has enabled showing others' loot/cast rols.
+                     * 
+                     * The && !(input.StartsWith("you")) is done as an alternative to using ^((?!you).).* in each Regex
+                     */
+                    !configuration.ShowOthersLootRoll && !(input.StartsWith("you")) && Localization.Get(ChatRegexStrings.OthersRollNeedOrGreed).IsMatch(input) ||
+                    !configuration.ShowOthersCastLot && !(input.StartsWith("you")) && Localization.Get(ChatRegexStrings.OthersCastLot).IsMatch(input) ||
+                    !configuration.ShowOthersObtain && !(input.StartsWith("you")) && Localization.Get(ChatRegexStrings.OthersObtain).IsMatch(input)
                 )
                 {
                     return true;
