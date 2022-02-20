8857dbbf5e4020960f1248e88578e174e7f54d78
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2022-02-19 20:18:27 -0800

    Settings refactor (#24)

---
+++ TidyChat/Settings/TabFooter.cs
@@ -0,0 +1,37 @@
+using ImGuiNET;
+using System.Diagnostics;
+
+namespace TidyChat
+{
+    public static class SettingsTabFooter
+    {
+		public static void Display(Configuration configuration, ref bool settingsVisible)
+		{
+			ImGui.Separator();
+			ImGui.Spacing();
+			if (ImGui.Button("Save and Close Config"))
+			{
+				configuration.Save();
+				settingsVisible = false;
+			}
+			if (!configuration.NoCoffee)
+			{
+				ImGui.SameLine();
+				ImGui.PushStyleColor(ImGuiCol.Button, 0xFF000000 | 0x005E5BFF);
+				ImGui.PushStyleColor(ImGuiCol.ButtonActive, 0xDD000000 | 0x005E5BFF);
+				ImGui.PushStyleColor(ImGuiCol.ButtonHovered, 0xAA000000 | 0x005E5BFF);
+
+				if (ImGui.Button("Support on Ko-Fi"))
+				{
+					Process.Start(new ProcessStartInfo
+					{
+						FileName = "https://ko-fi.com/nadyanayme",
+						UseShellExecute = true
+					});
+				}
+
+				ImGui.PopStyleColor(3);
+			}
+		}
+	}
+}
---
+++ TidyChat/Settings/Tabs/CraftingGatheringTab.cs
@@ -0,0 +1,109 @@
+using Dalamud.Interface.Components;
+using ImGuiNET;
+
+namespace TidyChat.Settings.Tabs
+{
+    internal static class CraftingGatheringTab
+    {
+        public static void Draw(Configuration configuration)
+        {
+            var filterCraftingSpam = configuration.FilterCraftingSpam;
+            if (ImGui.Checkbox("Filter all Crafting messages except \"You synthesize a/an <item>\"", ref filterCraftingSpam))
+            {
+                configuration.FilterCraftingSpam = filterCraftingSpam;
+                configuration.Save();
+            }
+            ImGuiComponents.HelpMarker("This allows you to use ChatAlerts to create an alert for \"You synthesize\" instead of using macro-finished alerts");
+
+            var filterGatheringSpam = configuration.FilterGatheringSpam;
+            if (ImGui.Checkbox("Filter spammy Gathering messages.", ref filterGatheringSpam))
+            {
+                configuration.FilterGatheringSpam = filterGatheringSpam;
+                configuration.Save();
+            }
+            ImGuiComponents.HelpMarker("Hide \"begin/finish\" gathering messages, as well as location affects");
+
+            ImGui.Separator();
+
+            if (ImGui.CollapsingHeader("Materia"))
+            {
+                var showAttachedMateria = configuration.ShowAttachedMateria;
+                if (ImGui.Checkbox("Show message when materia is succesfully attached to gear", ref showAttachedMateria))
+                {
+                    configuration.ShowAttachedMateria = showAttachedMateria;
+                    configuration.Save();
+                }
+
+                var showOvermeldFailure = configuration.ShowOvermeldFailure;
+                if (ImGui.Checkbox("Show message when materia fails to be overmelded onto gear", ref showOvermeldFailure))
+                {
+                    configuration.ShowOvermeldFailure = showOvermeldFailure;
+                    configuration.Save();
+                }
+
+                var showMateriaRetrieved = configuration.ShowMateriaRetrieved;
+                if (ImGui.Checkbox("Show message when you succesfully retrieve materia from gear", ref showMateriaRetrieved))
+                {
+                    configuration.ShowMateriaRetrieved = showMateriaRetrieved;
+                    configuration.Save();
+                }
+
+                var showMateriaShatters = configuration.ShowMateriaShatters;
+                if (ImGui.Checkbox("Show message when materia shatters during retrieval", ref showMateriaShatters))
+                {
+                    configuration.ShowMateriaShatters = showMateriaShatters;
+                    configuration.Save();
+                }
+
+                var showMateriaExtract = configuration.ShowMateriaExtract;
+                if (ImGui.Checkbox("Show message when materia is extracted from spiritbonded gear", ref showMateriaExtract))
+                {
+                    configuration.ShowMateriaExtract = showMateriaExtract;
+                    configuration.Save();
+                }
+            }
+
+            ImGui.Spacing();
+
+            if (ImGui.CollapsingHeader("Gathering Locations"))
+            {
+                var showAetherialReductionSands = configuration.ShowAetherialReductionSands;
+                if (ImGui.Checkbox("Show the number of obtained sands after using aetherial reduction", ref showAetherialReductionSands))
+                {
+                    configuration.ShowAetherialReductionSands = showAetherialReductionSands;
+                    configuration.Save();
+                }
+
+                var showLocationAffects = configuration.ShowLocationAffects;
+                if (ImGui.Checkbox("Show message when location affects gathering yield, receiving Gatherer's Boon, or gathering attempts", ref showLocationAffects))
+                {
+                    configuration.ShowLocationAffects = showLocationAffects;
+                    configuration.Save();
+                }
+
+                var hideGatheringYield = configuration.HideGatheringYield;
+                if (ImGui.Checkbox("Hide only locations that affect gathering yield", ref hideGatheringYield))
+                {
+                    configuration.HideGatheringYield = hideGatheringYield;
+                    configuration.Save();
+                }
+
+                var hideGatheringAttempts = configuration.HideGatheringAttempts;
+                if (ImGui.Checkbox("Hide only locations that affect gathering attempts", ref hideGatheringAttempts))
+                {
+                    configuration.HideGatheringAttempts = hideGatheringAttempts;
+                    configuration.Save();
+                }
+
+                var hideGatherersBoon = configuration.HideGatherersBoon;
+                if (ImGui.Checkbox("Hide only locations that affect Gatherer's Boon", ref hideGatherersBoon))
+                {
+                    configuration.HideGatherersBoon = hideGatherersBoon;
+                    configuration.Save();
+                }
+            }
+
+            ImGui.EndTabItem();
+        }
+    }
+}
