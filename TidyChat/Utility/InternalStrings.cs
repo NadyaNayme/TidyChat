c8813f441ce19fb542ff3f2017d823f6ad0cda8e
Nadya <4567166+NadyaNayme@users.noreply.github.com>
2022-03-15 23:14:34 -0700

    chore: version bump
    Signed-off-by: Nadya <4567166+NadyaNayme@users.noreply.github.com>

--- TidyChat/Utility/InternalStrings.cs
+++ TidyChat/Utility/InternalStrings.cs
@@ -2,7 +2,7 @@
 {
 	internal static class InternalStrings
 	{
-		public readonly static string Version = "0.1.1.10";
+		public readonly static string Version = "0.1.1.11";
 
 		public readonly static string PluginName = "Tidy Chat";
 
