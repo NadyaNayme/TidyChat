969ada4d6ada67fa765547134cb5bfd404c8e3c6
Kage <95875972+Kagekazu@users.noreply.github.com>
2026-06-05 14:48:58 +0200

    more UI

---
+++ TidyChat/Settings/Tabs/Tools/WhitelistTab.cs
@@ -0,0 +1,241 @@
+using Dalamud.Interface;
+using Dalamud.Interface.Components;
+using Dalamud.Interface.Utility;
+using System.Numerics;
+namespace TidyChat.Settings.Tabs;
+
+internal static class WhitelistTab
+{
+    private static PlayerName m_placeholder = new();
+
+    public static void DrawContent(Configuration configuration)
+    {
+        var sentByWhitelistPlayer = configuration.SentByWhitelistPlayer;
+        if (ImGui.Checkbox(Languages.WhitelistTab_ShowAllMessagesByWhitelistedPlayer, ref sentByWhitelistPlayer))
+        {
+            configuration.SentByWhitelistPlayer = sentByWhitelistPlayer;
+            configuration.OnSettingChanged();
+        }
+        ImGuiComponents.HelpMarker(Languages.WhitelistTab_ShowAllMessagesByWhitelistedPlayerHelpMarker);
+
+        var targetingWhitelistPlayer = configuration.TargetingWhitelistPlayer;
+        if (ImGui.Checkbox(Languages.WhitelistTab_ShowAllMessagesTargetingWhitelistedPlayer,
+                ref targetingWhitelistPlayer))
+        {
+            configuration.TargetingWhitelistPlayer = targetingWhitelistPlayer;
+            configuration.OnSettingChanged();
+        }
+        ImGuiComponents.HelpMarker(Languages.WhitelistTab_ShowAllMessagesTargetingWhitelistedPlayerHelpMarker);
+
+        ImGui.TextWrapped(Languages.WhitelistTab_ExplanationMessage);
+        ImGui.Spacing();
+        ImGui.TextWrapped(Languages.WhitelistTab_FilteringNote);
+        ImGui.Spacing();
+
+        ImGui.TextUnformatted(Languages.WhitelistTab_SelectChannelsHeader);
+        ImGui.SameLine();
+        ImGuiComponents.HelpMarker(Languages.WhitelistTab_ChannelsHelpMarker);
+        ImGui.SameLine();
+        ImGui.TextUnformatted(Languages.WhitelistTab_FiltersHeader);
+        ImGui.SameLine();
+        ImGuiComponents.HelpMarker(Languages.WhitelistTab_FiltersHelpMarker);
+        ImGui.SameLine();
+        ImGui.TextUnformatted(Languages.WhitelistTab_Allow);
+        ImGui.SameLine();
+        ImGuiComponents.HelpMarker(Languages.WhitelistTab_AllowBlockHelpMarker);
+        ImGui.Spacing();
+
+        var outer_height = new Vector2(640f, 400f);
+        if (!ImGui.BeginTable("##whitelistTable", 4,
+                ImGuiTableFlags.NoHostExtendX | ImGuiTableFlags.ScrollY | ImGuiTableFlags.Borders |
+                ImGuiTableFlags.RowBg, outer_height))
+        {
+            return;
+        }
+        ImGui.TableSetupScrollFreeze(0, 1);
+        ImGui.TableSetupColumn(Languages.WhitelistTab_SelectChannelsHeader, ImGuiTableColumnFlags.WidthFixed, 148f);
+        ImGui.TableSetupColumn(Languages.WhitelistTab_FiltersHeader, ImGuiTableColumnFlags.WidthStretch);
+        ImGui.TableSetupColumn(Languages.WhitelistTab_Allow, ImGuiTableColumnFlags.WidthFixed, 96f);
+        ImGui.TableSetupColumn("##DeleteColumn", ImGuiTableColumnFlags.WidthFixed, 36f);
+        ImGui.TableHeadersRow();
+        var list = configuration.Whitelist.ToList();
+        for (var i = -1; i < list.Count; i++)
+        {
+            ImGui.TableNextRow();
+            var alias = i < 0 ? m_placeholder : list[i];
+
+            #region Channels Column
+
+            ImGui.TableNextColumn();
+            ImGui.Spacing();
+            if (ImGui.CollapsingHeader($"{Languages.WhitelistTab_ChannelsHeader}##whitelist{i}ChannelsHeader"))
+            {
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_SystemChannel}##whitelist{i}OverrideSystemFilters",
+                        ref alias.WhitelistedChannels,
+                        1 << 3))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_TalkingChannel}##whitelist{i}OverrideTalkingFilters",
+                        ref alias.WhitelistedChannels,
+                        1 << 2))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_EmotesChannel}##whitelist{i}OverrideEmoteFilters",
+                        ref alias.WhitelistedChannels, 1 << 1))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags($"{Languages.ChatHistoryTab_LootChannel}##whitelist{i}OverrideLootFilters",
+                        ref alias.WhitelistedChannels, 1 << 5))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_CraftingChannel}##whitelist{i}OverrideCraftingFilters",
+                        ref alias.WhitelistedChannels,
+                        1 << 8))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_GatheringChannel}##whitelist{i}OverrideGatheringFilters",
+                        ref alias.WhitelistedChannels,
+                        1 << 9))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_LoginLogoutChannel}##whitelist{i}OverrideFreeCompanyFilters",
+                        ref alias.WhitelistedChannels, 1 << 7))
+                {
+                    configuration.OnSettingChanged();
+                }
+                if (ImGui.CheckboxFlags(
+                        $"{Languages.ChatHistoryTab_ProgressChannel}##whitelist{i}OverrideProgressFilters",
+                        ref alias.WhitelistedChannels,
+                        1 << 4))
+                {
+                    configuration.OnSettingChanged();
+                }
+            }
+
+            ImGui.Spacing();
+
+            #endregion Channels Column
+
+            #region Player Column
+
+            ImGui.TableNextColumn();
+            ImGui.Spacing();
+            if (i == -1)
+            {
+                ImGui.TextUnformatted(Languages.WhitelistTab_MessageContains);
+            }
+
+            ImGui.SetNextItemWidth(-1);
+            if (ImGui.InputText($"##whitelist{i}FirstNameInput", ref alias.FirstName, 120,
+                    ImGuiInputTextFlags.EnterReturnsTrue))
+            {
+                if (i == -1)
+                {
+                    configuration.Whitelist.Insert(0, alias);
+                    m_placeholder = new();
+                }
+
+                configuration.OnSettingChanged();
+            }
+
+            if (i != -1 && !alias.IsRegex)
+            {
+                var matchPreview = alias.MatchMode == PlayerNameMatchMode.ExactSender
+                    ? Languages.WhitelistTab_MatchModeExactSender
+                    : Languages.WhitelistTab_MatchModeMessageContains;
+                ImGui.SetNextItemWidth(-1);
+                if (ImGui.BeginCombo($"##whitelist{i}MatchMode", matchPreview))
+                {
+                    if (ImGui.Selectable(Languages.WhitelistTab_MatchModeMessageContains,
+                            alias.MatchMode == PlayerNameMatchMode.MessageContains))
+                    {
+                        alias.MatchMode = PlayerNameMatchMode.MessageContains;
+                        configuration.OnSettingChanged();
+                    }
+                    if (ImGui.Selectable(Languages.WhitelistTab_MatchModeExactSender,
+                            alias.MatchMode == PlayerNameMatchMode.ExactSender))
+                    {
+                        alias.MatchMode = PlayerNameMatchMode.ExactSender;
+                        configuration.OnSettingChanged();
+                    }
+                    ImGui.EndCombo();
+                }
+            }
+
+            ImGuiHelpers.ScaledDummy(10f);
+
+            #endregion Player Column
+
+            #region Allow Column
+
+            ImGui.TableNextColumn();
+            ImGui.Spacing();
+            if (i == -1)
+            { }
+            else
+            {
+                var previewValue = "";
+                if (alias.AllowMessage)
+                {
+                    previewValue = Languages.WhitelistTab_Allow;
+                }
+                else
+                {
+                    previewValue = Languages.WhitelistTab_Block;
+                }
+                ImGui.SetNextItemWidth(-1);
+                if (ImGui.BeginCombo($"##whitelist{i}AllowSetting", previewValue))
+                {
+                    if (ImGui.Selectable($"{Languages.WhitelistTab_Allow}##{i}", alias.AllowMessage))
+                    {
+                        alias.AllowMessage = true;
+                        configuration.OnSettingChanged();
+                    }
+
+                    if (ImGui.Selectable($"{Languages.WhitelistTab_Block}##{i}", !alias.AllowMessage))
+                    {
+                        alias.AllowMessage = false;
+                        configuration.OnSettingChanged();
+                    }
+
+                    ImGui.EndCombo();
+                }
+            }
+
+            #endregion Allow Column
+
+
+            #region Delete Column
+
+            ImGui.TableNextColumn();
+            ImGui.Spacing();
+            ImGui.PushID($"Delete{i}");
+            if (i != -1 && ImGuiComponents.IconButton(FontAwesomeIcon.Trash))
+            {
+                configuration.Whitelist.Remove(list[i]);
+                configuration.OnSettingChanged();
+            }
+
+            ImGui.PopID();
+
+            #endregion Delete Column
+        }
+
+        ImGui.EndTable();
+        ImGui.NewLine();
+        ImGui.Spacing();
+        ImGui.TextWrapped(Languages.WhitelistTab_ExactNameMatchWhitelistExplanation);
+    }
+}
